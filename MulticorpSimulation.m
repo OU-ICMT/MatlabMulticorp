923c09f15edf3d49d4e78ed6caf2cb41a2bc0594
Zhengchao Tian <zhengchaotian@gmail.com>
2016-10-13 12:25:38 -0400

    New changes.

--- MulticorpSimulation.m
+++ MulticorpSimulation.m
@@ -1,11 +1,12 @@
 clear;
 clc;
 % Load assembly
-asm_mc_base = NET.addAssembly('C:\Users\sarkara1\Documents\MATLABMulticorp\mc-base.dll');
-asm_mc_corrosioncase = NET.addAssembly('C:\Users\sarkara1\Documents\MATLABMulticorp\mc-corrosioncase.dll');
-asm_mc_util = NET.addAssembly('C:\Users\sarkara1\Documents\MATLABMulticorp\mc-util.dll');
-asm_mc_modeling = NET.addAssembly('C:\Users\sarkara1\Documents\MATLABMulticorp\mc-modeling.dll');
-asm_mc_corrosion = NET.addAssembly('C:\Users\sarkara1\Documents\MATLABMulticorp\mc-corrosion.dll');
+asm_mc_base = NET.addAssembly('C:\Users\zt359416\Documents\MATLAB\MatlabMulticorp\mc-base.dll');
+asm_mc_corrosioncase = NET.addAssembly('C:\Users\zt359416\Documents\MATLAB\MatlabMulticorp\mc-corrosioncase.dll');
+asm_mc_util = NET.addAssembly('C:\Users\zt359416\Documents\MATLAB\MatlabMulticorp\mc-util.dll');
+asm_mc_modeling = NET.addAssembly('C:\Users\zt359416\Documents\MATLAB\MatlabMulticorp\mc-modeling.dll');
+asm_mc_corrosion = NET.addAssembly('C:\Users\zt359416\Documents\MATLAB\MatlabMulticorp\mc-corrosion.dll');
+
 
 % Import namespace
 import edu.ohiou.icmt.multicorp.factory.*
@@ -39,7 +40,7 @@ newCase.onFlowTypeChanged();
 
 %choose point run
 simuType = newCase.getParameter(NameList.SIMU_TYPE);
-corrosionType.setOption(1);
+simuType.setOption(1);
 newCase.onSimulationTypeChanged();
 
 %Calculate composition
@@ -55,4 +56,13 @@ corrosionResult = newCase.getModel(NameList.MODEL_NAME_CORROSION_RESULT);
 
 simulation = newCase.getModel(NameList.MODEL_NAME_CORROSION_MODEL);
 simulation.makeOutputOptionList();
+newCase.generateCaseFile();
+%simulation.startSimulation();
 
+% while 1
+%     if (simulation.wasSimulationSuccess)
+%         break;
+%     end
+% end
+
+disp('Done')
